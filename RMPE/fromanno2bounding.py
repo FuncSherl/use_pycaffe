0a3f964c0f887d5e55aacfa28eb862713beae5b2
root <root@sherl-desktop>
2018-04-17 16:21:42 +0800

    don

--- RMPE/fromanno2bounding.py
+++ RMPE/fromanno2bounding.py
@@ -15,26 +15,27 @@ imgdir=op.join(rootdir, 'Images')
 
 
 outputdir='/media/sherl/本地磁盘/FashionAI/hdf5_train'
-
-def pro_line(inpu):#process a annotation line return [filedir, xmin, ymin, xmax, ymax]
+#Images/trousers/67a24d0b384d1355a71b80ece884c29a.jpg,trousers,-1_-1_-1,-1_-1_-1,-1_-1_-1,-1_-1_-1,-1_-1_-1,-1_-1_-1,-1_-1_-1,-1_-1_-1,-1_-1_-1,-1_-1_-1,-1_-1_-1,-1_-1_-1,-1_-1_-1,-1_-1_-1,-1_-1_-1,96_72_1,285_68_1,-1_-1_-1,-1_-1_-1,206_257_1,203_312_1,64_289_1,204_297_1,335_256_1
+def pro_line(inpu):#process a annotation line return filedir,type, [xmin, ymin, xmax, ymax]
     xmin=ymin=100000
     xmax=ymax=0
-    for i in inpu[2:]:
+    for i in inpu[2:]:#rid the first 2 col
         tep=map(int,i.split('_'))
         print tep
         if tep[-1]!=-1:
             if xmax<tep[0]:  xmax=tep[0]
             if xmin>tep[0]:  xmin=tep[0]
+            
             if ymin>tep[1]:  ymin=tep[1]
             if ymax<tep[1]:  ymax=tep[1]
-    return [inpu[0], xmin, ymin, xmax ,ymax]
+    return inpu[0],input[1], [xmin, ymin, xmax ,ymax]
 
 def pro_annotate(inpu):#form a map with key =dir
     ret={}
     reader = csv.reader(file(inpu, 'rb'))
     for i in reader:
-        tep=pro_line(i)
-        ret[tep[0]]=tep[1:]
+        dirs,typ,tep=pro_line(i)
+        ret[dirs]=tep
     
     return ret
         
